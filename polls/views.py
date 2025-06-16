20b030c1f2c46f7a3c55553c694a75736c7d939c
lokesh <lokesh.saini@dianapps.com>
2025-06-16 13:02:43 +0530

    dynamic url handled

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
 from .models import Question
 
@@ -12,12 +12,17 @@ def index(request):
     # The render() function takes the request object as its first argument, a template name as its second argument and a dictionary as its optional third argument. It returns an HttpResponse object of the given template rendered with the given context.
 
 
+# def detail(request,question_id):
+#     # return HttpResponse(f"You are looking at the question {question_id}")
+#     try:
+#         question=Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request,"polls/detail.html",{"question":question})
+
 def detail(request,question_id):
     # return HttpResponse(f"You are looking at the question {question_id}")
-    try:
-        question=Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question=get_object_or_404(Question,pk=question_id)
     return render(request,"polls/detail.html",{"question":question})
 
 def results(request,question_id):
